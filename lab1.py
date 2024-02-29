e5876a664252c9cff94bb890f50eface71bef209
AntoniosSoula <161150981+AntoniosSoula@users.noreply.github.com>
2024-02-29 17:23:35 +0200

    lab1.py

--- lab1.py
+++ lab1.py
@@ -4,23 +4,23 @@ def get_server_info(url):
     try:
         response = requests.head(url)
         server_info = response.headers.get('Server', 'Server information not available')
-        print(f"(α) Λογισμικό εξυπηρετητή: {server_info}")
-
+        print(f"Λογισμικό εξυπηρετητή: {server_info}")
         cookies = response.cookies
+
         if cookies:
-            print("(β) Η σελίδα χρησιμοποιεί cookies.")
-            print("(γ) Πληροφορίες για τα cookies:")
+            print("Η σελίδα χρησιμοποιεί cookies.")
+            print("Πληροφορίες για τα cookies:")
             for cookie in cookies:
                 cookie_name = cookie.name
                 cookie_expires = "Δεν καθορίζεται" if cookie.expires is None else str(cookie.expires)
-                print(f"  - Όνομα: {cookie_name}, Διάρκεια Εγκυρότητας: {cookie_expires}")
+                print(f"  - Όνομα: {cookie_name}, Διάστημα Εγκυρότητας: {cookie_expires}")
         else:
-            print("(β) Η σελίδα δεν χρησιμοποιεί cookies.")
+            print("Η σελίδα δεν χρησιμοποιεί cookies.")
     except requests.exceptions.RequestException as e:
-        print(f"Σφάλμα κατά την επικοινωνία με το URL: {e}")
+        print(f"Σφάλμα με το URL: {e}")
 
 def main():
-    user_url = input("Εισάγετε το URL: ")
+    user_url = input("URL: ")
     get_server_info(user_url)
 
 if __name__ == "__main__":
